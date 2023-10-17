55e2be58964d69b92f93a612646773fb31af9170
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2023-10-17 13:33:08 +0200

    /login snart klar
    måste kolla upp en grej med objekten bara....

--- apps/server/src/routes/userRoutes.js
+++ apps/server/src/routes/userRoutes.js
@@ -4,6 +4,32 @@ import { end_user } from '../objects/end_user.js';
 export const getUserRoutes = (db) => {
     const router = Router();
 
+    router.post('/login', async (req, res, next) => {
+        /*
+
+        */
+        const result = await end_user.findOne({
+            where : {
+                email : req.body.email
+            }
+        });
+
+        if (result === null) {
+            res.status(404).json('Username or password incorrect'); //TODO: ändra felmeddelandet alternativt skriv det någon annanstans?
+        }
+
+        if (req.body.password === result.password) {
+            res.status(200).json(
+                {
+                    id : result.id
+                }
+            );
+        }
+        else {
+            res.status(400).json('Username or password incorrect'); //TODO: ändra felmeddelandet alternativt skriv det någon annanstans?
+        }
+    })
+
     router.post('/register', async (req, res, next) => {
     /*
     Registers a user in the DB using the end_user class
