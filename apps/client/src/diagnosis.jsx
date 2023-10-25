79d43dfe58568e9f3a39195ac7b2a6a7c85bbe6a
Esaiasson <teo.esaiasson@gmail.com>
2023-10-25 21:57:40 +0200

    handle correct answer on diagnosis

--- apps/client/src/diagnosis.jsx
+++ apps/client/src/diagnosis.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import { useEffect } from "react";
-
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    CardBody,
     Collapse,
-    Card,
     Button,
-    Heading
+    Heading,
+    Input,
+    Box,
+    Text,
+    Flex
 } from '@chakra-ui/react'
 import { useDisclosure } from "@chakra-ui/react";
 
@@ -19,6 +16,8 @@ import { useDisclosure } from "@chakra-ui/react";
 export default function Diagnosis(props) {
 
     const [stepData, setStepData] = useState({});
+    const [diagnosisList, setDiagnosisList] = useState([])
+    const [diagnosisListHtml, setDiagnosisListHtml] = useState("")
     const [feedbackToDisplay, setFeedbackToDisplay] = useState();
     const { isOpen, onToggle } = useDisclosure();
 
@@ -27,10 +26,9 @@ export default function Diagnosis(props) {
         const fetchStep = async () => {
             const headers = {
                 "Content-type" : "application/json",
-                "id": 'dce351f8-db4e-4e91-a1d2-a5dc12f090c7'
+                "id": 'dce351f8-db4e-4e91-a1d2-a5dc12f090c7'            //hårdkodar detta step-id för tillfället:
             }
             
-            //hårdkodar detta step-id för tillfället:
     
             const response = await props.getCallToApi('http://localhost:5173/api/case/getDiagnosisStep', headers);
 
@@ -44,13 +42,56 @@ export default function Diagnosis(props) {
             });
         }
 
+        const fetchDiagnosisList = async () => {
+            const headers = {
+                "Content-type" : "application/json",
+                "id": '6ff9e368-1e44-46d1-8192-ac3069c34f20'            //hårdkodar detta medical_field_id för tillfället:
+            }
+            
+    
+            const response = await props.getCallToApi('http://localhost:5173/api/case/getDiagnosisList', headers);
+            setDiagnosisList(response)
+        }
+
+        props.setDisplayFeedback(false);
+
         fetchStep();
+        fetchDiagnosisList();
     }, []);
 
+    const findDiagnosis = async(searchString) => {
+        /* Filters the diagnosis list based on the param search string  */
+        if(searchString.length > 0){
+            var filterdList = diagnosisList.filter(obj => {
+            return obj.name.toLowerCase().includes(searchString.toLowerCase());
+            })
+        } else{
+            filterdList = [{}]
+        }
+  
+        setDiagnosisListHtml(
+            filterdList.map((caseItem) => (
+                <Box key={caseItem.id}>
+                    {caseItem.name}
+                    <Button onClick={(e) => handleFeedback(caseItem.id)}>Ställ diagnos</Button>
+                </Box>
 
-    useEffect(() => {
-        console.log(stepData)
-    }, [stepData]);
+            ))
+        )
+
+    }
+
+    const handleFeedback = (choosenDiagnosId) => {
+        props.setDisplayFeedback(true);
+
+        if(choosenDiagnosId == stepData.id){
+            setFeedbackToDisplay(stepData.feedback_correct);
+        }
+
+        if(choosenDiagnosId != stepData.id){
+            setFeedbackToDisplay(stepData.feedback_incorrect);
+        }
+    }
 
     return ( 
         <div>
@@ -59,20 +100,16 @@ export default function Diagnosis(props) {
             <Collapse in={isOpen} startingHeight={30} noOfLines={1}>
                 {stepData.prompt}
             </Collapse>
+            {isOpen == false && 
             <Button onClick={onToggle}>Visa mer</Button>
+            }
+            {isOpen && 
+            <Button onClick={onToggle}>Visa mindre</Button>
+            }
 
-
-            <Accordion allowToggle>
-
-                    <AccordionItem key={prompt}>
-                            <AccordionButton>
-                                <h2>{stepData.prompt}</h2>
-                            </AccordionButton>
-                            <AccordionPanel pb={4}>
-                            </AccordionPanel>
-                    </AccordionItem>
-
-            </Accordion>
+            <Input onChange={(e) => findDiagnosis(e.target.value)} placeholder='Skriv din diagnos här'/>
+            {diagnosisListHtml}
+            {feedbackToDisplay}
         </div>
     )
 }
--- apps/server/src/routes/caseRoutes.js
+++ apps/server/src/routes/caseRoutes.js
@@ -82,6 +82,16 @@ export const getCaseRoutes = (db) => {
         });
         res.status(200).json(result);
     })
+
+    router.get('/getDiagnosisList', async(req,res,next)=>{
+        const result = await object.diagnosis_list.findAll({
+            where: {
+                medical_field_id: req.header('id')
+            }
+        });
+        res.status(200).json(result);
+    })
+
     //Hämta specifict Treatment step
     router.get('/getTreatmentStep', async(req,res,next)=>{
         const result = await object.treatment.findAll({
