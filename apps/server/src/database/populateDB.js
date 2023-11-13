56871a40d161b50333d809536c149887c0af0555
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2023-11-13 14:19:15 +0100

    satteupp ett skelett
    Co-Authored-By: Esaiasson <100272341+Esaiasson@users.noreply.github.com>

--- apps/server/src/database/populateDB.js
+++ apps/server/src/database/populateDB.js
@@ -4,12 +4,51 @@ import { db } from '../database/databaseConnection.js';
 
 
 export const populateDB = async () => {
-    db.models.end_user.create({
-        email: '1',
-        password: '1',
-        is_admin: true,
-        group_id: 'admin'
-    })
+    db.models.end_user.bulkCreate([
+        {
+            email: '1',
+            password: '1',
+            is_admin: true,
+            group_id: 'admin'
+        },
+        {
+           email: '2',
+           password: '2',
+           is_admin: false,
+           group_id: 'user'
+        }
+    ])
+
+    //examination_type
+    //examination_subtype
+    //examination_list
+    //examination
+    //examination_specific_values
+
+    //treatment_type
+    //treatment_subtype
+    //treatment_list
+    //treatment
+    //step_specific_treatment
+
+    //medical_field
+    //diagnosis_list
+    //diagnosis
+
+    //introduction
+    //summary
+
+    //module_type
+
+    //medical_case
+
+    //step
+    //attempt??
+
+
+    
+
+
 }
 
 
