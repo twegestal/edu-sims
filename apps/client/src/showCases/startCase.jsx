3622fc9d20da2d405ad6fd48ccd61e07b66a91c6
Max <max.svedala@gmail.com>
2023-12-19 10:21:47 +0100

    Fixat så localstorage har senaste attemptet
    Fixat så en array i localstorage, finns med senaste attemptet

--- apps/client/src/showCases/startCase.jsx
+++ apps/client/src/showCases/startCase.jsx
@@ -1,4 +1,4 @@
-import { 
+import {
   Button,
   Progress
 } from '@chakra-ui/react';
@@ -14,10 +14,40 @@ export default function StartCase(props) {
   const postToAttemptNoReload = async () => {
     let createdAttempt = [];
     createdAttempt = await createAttempt(user.id, props.caseId);
+    let startedCases = JSON.parse(localStorage.getItem('StartedCases'));
+    console.log(startedCases);
+    if (startedCases !== null) {
+      let value = startedCases.length;
+      for (let index = 0; index < startedCases.length; index++) {
+        if (startedCases[index].caseId === props.caseId) {
+          value = index;
+          break;
+        }
+      }
+      if (value === startedCases.length) {
+        const attempLocal = {}
+        attempLocal.attempt = createdAttempt.id;
+        attempLocal.caseId = props.caseId;
+        startedCases[value] = attempLocal;
+      } else {
+        startedCases[value].attempt = createdAttempt.id;
+        startedCases[value].caseId = props.caseId;
+      }
+      localStorage.setItem('StartedCases', JSON.stringify(startedCases));
+    } else {
+      const arr = [];
+      const attempLocal = {}
+      attempLocal.attempt = createdAttempt.id;
+      attempLocal.caseId = props.caseId;
+      console.log(attempLocal);
+      arr[0] = attempLocal;
+      localStorage.setItem('StartedCases', JSON.stringify(arr));
+    }
     return navigate(
       '/case/caseid=' + props.caseId + '/attemptid=' + createdAttempt.id + '/reload=' + false,
     );
   };
+
   const postToAttemptReload = async () => {
     let createdAttempt = [];
     createdAttempt = await createAttempt(user.id, props.caseId);
