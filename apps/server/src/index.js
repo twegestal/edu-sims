8d90191a57c6a54154e7d4aa637623a51a1e2bb8
twegestal <96296150+twegestal@users.noreply.github.com>
2024-01-24 14:23:20 +0100

    Test PR

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -6,6 +6,8 @@ const port = 443;
 
 const server = createServer();
 
+console.log('test') //REMOVE THIS
+
 const privateKey = fs.readFileSync(process.env.PATH_TO_KEY, 'utf8');
 const certificate = fs.readFileSync(process.env.PATH_TO_CERT, 'utf8');
 
